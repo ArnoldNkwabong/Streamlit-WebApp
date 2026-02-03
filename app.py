20ea57260c9c6ea796247400fd744e582d97caa5
Arnold Nkwabong <jessyarnoldnkwabong@gmail.com>
2026-02-03 16:06:38 +0100

    Update app.py

--- app.py
+++ app.py
@@ -1,5 +1,5 @@
 import streamlit as st
-import leafmap.folium as leafmap
+import leafmap.foliumap as leafmap
 
 st.set_page_config(page_title='My Webpage', page_icon=':tada:', layout='wide')
 
@@ -27,6 +27,7 @@ m = leafmap.Map()
 m.split_map(left_layer="ESA WorldCover 2020 S2 FCC", right_layer="ESA WorldCover 2020")
 m.add_legend(title="ESA Land Cover", builtin_legend="ESA_WorldCover")
 m.to_streamlit(height=700)
+
 
 
 
