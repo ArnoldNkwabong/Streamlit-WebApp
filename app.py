9e1a7b508faa44f84c6153e3c004918b88ef1132
Arnold Nkwabong <jessyarnoldnkwabong@gmail.com>
2026-02-03 15:16:19 +0100

    Add files via upload

---
+++ app.py
@@ -0,0 +1,25 @@
+import streamlit as st
+
+st.set_page_config(page_title='My Webpage', page_icon=':tada:', layout='wide')
+
+#  Customize the sidebar
+markdown = """
+Web App URL: <https://geotemplate.streamlit.app>
+GitHub Repository: <https://github.com/giswqs/streamlit-multipage-template>
+"""
+
+st.sidebar.title("About")
+st.sidebar.info(markdown)
+logo = "https://i.imgur.com/UbOXYAU.png"
+st.sidebar.image(logo)
+
+# Customize page title
+st.title("Streamlit for Geospatial Applications")
+
+st.markdown(
+    """
+    This multipage app template demonstrates various interactive web apps created using [streamlit](https://streamlit.io) and [leafmap](https://leafmap.org). It is an open-source project and you are very welcome to contribute to the [GitHub repository](https://github.com/giswqs/streamlit-multipage-template).
+    """
+)
+
+st.header("Instructions")
